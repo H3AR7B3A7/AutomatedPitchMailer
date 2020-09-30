1ae9fefd972ab3a5c2f90dc2a0e517efa821f7e6
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-09-30 23:58:41 +0200

    Code clean up

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,40 +11,19 @@ public class Main {
     static String myName;
 
     public static void main(String[] args) throws IOException {
-        if(args.length > 0) {
-            addressedName = args[0];
-        }
-        if(args.length > 1) {
-            company = args[1];
-        }
-        if(args.length > 2) {
-            bestQuality = args[2];
-        }
-        if(args.length > 3) {
-            myName = args[3];
-        }
+        argumentCheck(args);
+        getInfo(args);
+        writePitch();
+    }
 
-        if(args.length < 4) {
-
-            BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("The name of the addressed:");
-            System.out.println("Press enter if the addressed is unknown.");
-            addressedName = cin.readLine();
-            System.out.println("The company name:");
-            company = cin.readLine();
-
-            System.out.println("Your best quality:");
-            bestQuality = scanner.nextLine();
-            System.out.println("Your name:");
-            myName = scanner.nextLine();
-        }
-
+    /**
+     * Write the pitch
+     */
+    private static void writePitch() throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("pitch.txt"));
-        if(addressedName.equals("")){
+        if (addressedName.equals("")) {
             writer.append("Hi,\n");
-        }else{
+        } else {
             writer.append("Dear ");
             writer.append(addressedName);
             writer.append(",\n");
@@ -66,5 +45,44 @@ public class Main {
         printWriter = new PrintWriter(new FileWriter("pitch.txt", true));
         printWriter.printf("Kind regards,\n\n\n%s", myName);
         printWriter.close();
+    }
+
+    /**
+     * Ask for values if arguments are not all present
+     */
+    private static void getInfo(String[] args) throws IOException {
+        if (args.length < 4) {
+            BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("The name of the addressed:");
+            System.out.println("Press enter if the addressed is unknown.");
+            addressedName = cin.readLine();
+            System.out.println("The company name:");
+            company = cin.readLine();
+
+            System.out.println("Your best quality:");
+            bestQuality = scanner.nextLine();
+            System.out.println("Your name:");
+            myName = scanner.nextLine();
+        }
+    }
+
+    /**
+     * Check for arguments
+     */
+    private static void argumentCheck(String[] args) {
+        if (args.length > 0) {
+            addressedName = args[0];
+        }
+        if (args.length > 1) {
+            company = args[1];
+        }
+        if (args.length > 2) {
+            bestQuality = args[2];
+        }
+        if (args.length > 3) {
+            myName = args[3];
+        }
     }
 }
